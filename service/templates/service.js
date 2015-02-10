f3e1656a2a12e78cb2bb22e31b38c36bd339b46a
Rob Riggs <robr24@gmail.com>
2015-02-10 11:24:13 -0500

    feat(service): utilize service for http services, sample snippet included

--- service/templates/service.js
+++ service/templates/service.js
@@ -1,6 +1,27 @@
-angular.module('<%= appname %>').factory('<%= _.camelize(name) %>',function() {
+angular.module('<%= appname %>').service('<%= _.camelize(name) %>', ['$http', '$q', function($http, $q) {
 
-    var <%= _.camelize(name) %> = {};
+  var SERVICE_URL_PREFIX = 'http://localhost:9000';
 
-    return <%= _.camelize(name) %>;
-});
+  /**
+   * function getData
+   * service call pattern using angular $q promises
+   * resolve returns response, reject returns error
+   * @returns {promise.promise|jQuery.promise}
+   */
+  this.getData = function() {
+    var deferred = $q.defer();
+    var url = SERVICE_URL_PREFIX;
+
+    $http.get(url)
+      .success(function (results) {
+        var data = results || [];
+        deferred.resolve(data);
+      })
+      .error(function (error) {
+        deferred.reject (error);
+      });
+
+    return deferred.promise;
+  };
+
+}]);
